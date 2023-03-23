b2f67047d24b630978e4a1b5378715036f6b05b7
jamashita <jamashita@arachnid.xyz>
2023-03-23 18:23:17 +0900

    feat: 🎸 NegationProbability

--- src/probability/index.ts
+++ src/probability/index.ts
@@ -1,5 +1,6 @@
 export * from './CompositeOpportunity.js';
 export * from './DecimalOpportunity.js';
 export * from './FractionOpportunity.js';
+export * from './NegationOpportunity.js';
 export * from './Opportunity.js';
 export * from './Probability.js';
