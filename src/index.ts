422a1c6452f7e5cd5bda9629a78e97bb38f326c1
jamashita <jamashita@arachnid.xyz>
2023-07-17 17:14:07 +0900

    feat: 🎸 http status

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './arithmetic/index.js';
 export * from './config/index.js';
 export * from './delay/index.js';
 export * from './digest/index.js';
+export * from './httpStatus/index.js';
 export * from './json/index.js';
 export * from './probability/index.js';
 export * from './random/index.js';
